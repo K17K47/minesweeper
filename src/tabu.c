3f035dcca8735cb9fb8daa559a855b74fbb9d536
joaohenr <joao.henrique2@live.com>
2016-06-14 16:29:07 -0300

    imprimir tabuleiro

---
+++ src/tabu.c
@@ -0,0 +1,21 @@
+void imprimirTabuleiro(tabuleiro_T tab){
+
+	int i, j;
+   vec v;
+  
+     for( j = 0; j < get_n(tab)+ 1; j++) {
+       if(j) printf("\t%i",j);
+        v.x=j;
+	 
+		for( i = 0; i < get_m(tab); i++){  
+         v.y=i;
+         
+          if(j) printf("     \t/%c/", get_usr(tab,v)  );
+           if(!j) printf("\t\t%i",i+1);
+		}
+		printf("\n");
+	}
+
+    
+}
+
