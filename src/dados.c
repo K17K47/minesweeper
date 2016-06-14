78387a3aa34f319a2fa83343f7332ad8483c163b
K17K47 <k17k47@gmail.com>
2016-06-14 16:25:41 -0300

    Correção de formatação e de pequenos erros

--- src/dados.c
+++ src/dados.c
@@ -1,23 +1,5 @@
 #include "dados.h"
 
-typedef struct {
-unsigned m;
-unsigned n;
-unsigned q;
-char* usr;
-char* gabarito;
-} tabuleiro_t;
-
-typedef struct {
-unsigned x;
-unsigned y;
-} vec;
-
-typedef struct {
-vec v;	//como escrever ? "v" ou "coord" ?
-char opjogada
-} jogada_t;
-
 unsigned get_m(tabuleiro_t tab)
 {
 	return tab.m;
@@ -68,15 +50,15 @@ void set_gabarito(tabuleiro_t* tab, vec v, char gabarito)
 	tab->gabarito[tab->m*v.x + v.y]=gabarito;
 }
 
-void inicializatab(tabuleiro_t* tab)
+void inicializa_tab(tabuleiro_t* tab)
 {
 	tab->usr = malloc (m*n);
 	tab->gabarito = malloc (m*n);
 }
-void destroitab(tabuleiro_t* tab)
+void destroi_tab(tabuleiro_t* tab)
 {
-	free tab->user;
-	free tab->gabarito;
+	free(tab->user);
+	free(tab->gabarito);
 }
 
 char get_jogada(jogada_t jogada)
@@ -91,7 +73,7 @@ void set_jogada(jogada_t* jogada, char op);	//op = opção
 
 vec get_coord (jogada_t jogada)
 {
-	return vec;
+	return jogada.vec;
 }
 
 void set_coord (jogada_t jogada, unsigned x, unsigned y)
