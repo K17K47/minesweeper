0d785c533ed4f5b04978b06afce77e77a8bb6215
joaohenr <joao.henrique2@live.com>
2016-06-19 16:56:39 -0300

    funçao ler jogada pronta

--- src/ui.c
+++ src/ui.c
@@ -17,3 +17,44 @@ void imprimirTabuleiro(tabuleiro_t tab){
    }
 }
 
+
+
+void Lerjogada ( jogada_t*jog , tabuleiro_t tab ) {
+
+ unsigned x , y ;
+ char op ;
+
+ printf(" Digite a coordenada e a opcao de jogada " );
+ scanf("%u%u",&x,&y);
+ op=getchar();
+   while( getchar() != ’\n’ );
+ 
+
+ if( ( !(x <= get_n(tab) || !(y <= get_m(tab) ) && ( !(op == v) || !(op == m) || !( op == d )  ) ) {
+
+    printf(" Erro na coordenada e na opcao de jogada. Digite novamente ") ;
+    scanf("%u%u",&x,&y);
+    op=getchar();
+     while( getchar() != ’\n’ );
+
+}
+  while ( !(x <= get_n(tab) || !(y <= get_m(tab)  ){
+
+      printf(" Erro na coordenada . digite novamente " );
+      scanf("%u%u",&x,&y);
+        while( getchar() != ’\n’ );
+     
+}
+  while ( !(op == v) || !(op == m) || !( op == d ) ) {
+       
+      printf(" Erro na opao de jogo . digite novamente " );
+      
+      op=getchar();
+        while( getchar() != ’\n’ );
+}
+
+  set_coord ( jog , x,  y);
+  set_jogada( jog , op);
+      
+   
+}
