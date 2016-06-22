18815502346257c2245ce2acd3a0e5ea0a524c8a
LordMomo <henriquetor@terra.com.br>
2016-06-22 18:45:25 -0300

    Correções no uso do tabuleiro

--- src/minesweeper.c
+++ src/minesweeper.c
@@ -4,8 +4,7 @@
 
 int main()
 {
-	tabuleiro_t gabarito;
-	tabuleiro_t usr;
+	tabuleiro_t tab;
 	jogada_t jog;
 	vec v;
 	int i, p = 0;
@@ -13,30 +12,28 @@ int main()
 	do
 	{
 		//menu
-		inicializa_tab( &gabarito );
-		inicializa_tab( &usr );
-		for(i = 0; i < get_q( gabarito ); i++) //colocar bombas
+		inicializa_tab( &tab );
+		CriarTabuleiroUsr( &tab )
+		for(i = 0; i < get_q( tab ); i++) //colocar bombas
 		{
-			ColocarBombas( &gabarito );
+			ColocarBombas( &tab );
 		}		
-		AvaliarVizinhos( &gabarito );
+		AvaliarVizinhos( &tab );
 		while( get_jogada( jogada ) != 'x' && get_jogada( jogada ) != 'r' )
 		{
-			imprimirTabuleiro( usr );
-			Lerjogada( &jog , usr );
-			if( ( get_jogada( jogada ) == 'v' ) && ( get_gabarito( gabarito, get_coord( jogada ) ) == '0' ) )
+			imprimirTabuleiro( tab );
+			Lerjogada( &jog , tab );
+			if( ( get_jogada( jogada ) == 'v' ) && ( get_gabarito( tab, get_coord( jogada ) ) == '0' ) )
 			{
-				Revela( &gabarito, &usr, get_coord( jogada ) );
+				Revela( &tab, get_coord( jogada ) );
 			}
-			p = p + ExecutaJogada( gabarito, &usr, &jogada );
-			if( p = get_q( gabarito ) )//caso em que o jogador marcou todas as minas
+			p = p + ExecutaJogada( &tab, &jogada );
+			if( p = get_q( tab ) )//caso em que o jogador marcou todas as minas
 			{
 				set_jogada( &jog, 'x');//fim de jogo - vitória
 			}
 		}
-		destroi_tab( &gabarito );
-		destroi_tab( &usr );
-
+		destroi_tab( &tab );
 	}while( get_jogada( jogada ) != 'x' );
 
 	//imprimir pontuação
