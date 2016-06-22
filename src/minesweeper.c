9a0922a2e2ed0b3e3915b8429307c767000f3037
LordMomo <henriquetor@terra.com.br>
2016-06-22 06:11:54 -0300

    Esboço da Main

--- src/minesweeper.c
+++ src/minesweeper.c
@@ -1,3 +1,41 @@
 #include "mech.h"
 #include "dados.h"
 #include "ui.h"
+
+int main()
+{
+	tabuleiro_t gabarito;
+	tabuleiro_t usr;
+	jogada_t jog;
+	vec v;
+	int i, p = 0;
+	
+	do
+	{
+		//menu
+		//inicializar tabuleiros
+		for(i = 0; i < get_q( gabarito ); i++) //colocar bombas
+		{
+			ColocarBombas( &gabarito );
+		}		
+		AvaliarVizinhos( &gabarito );
+		while( get_jogada( jogada ) != 'x' && get_jogada( jogada ) != 'r' )
+		{
+			imprimirTabuleiro( usr );
+			Lerjogada( &jog , usr );
+			if( ( get_jogada( jogada ) == 'q' ) && ( get_gabarito( gabarito, get_coord( jogada ) ) == '0' ) )
+			{
+				Revela( &gabarito, &usr, get_coord( jogada ) );
+			}
+			p = p + ExecutaJogada( gabarito, &usr, &jogada );
+		}
+		//destruir tabuleiros
+
+	}while( get_jogada( jogada ) != 'x' );
+	
+
+	//imprimir pontuação
+	//e se ele quiser jogar novamente?
+
+	return 0;
+}
